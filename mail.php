a89d71f85b80b21f734b21c4c42a00f25e8631b8
Kirill <kirill@webitmaster.ru>
2016-05-16 09:39:48 +0700

    init

---
+++ mail.php
@@ -0,0 +1,11 @@
+<?php
+
+$recepient = "escape6661@gmail.com";
+$sitename = "Название сайта";
+
+$name = trim($_GET["name"]);
+$phone = trim($_GET["phone"]);
+
+$pagetitle = "Новая заявка с сайта \"$sitename\"";
+$message = "Имя: $name \nТелефон: $phone";
+mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
